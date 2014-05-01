bb13d46ee521b574629d7a14ab7276cf7a9e76a7
James Carthew <jamesrobertcarthew@gmail.com>
2014-05-02 09:45:44 +1000

    2nd commit - basic structure outline

--- cpid_server.py
+++ cpid_server.py
@@ -10,6 +10,19 @@ config.read('gains.conf')
 kP = config.get('gains', 'kP')
 kI = config.get('gains', 'kI')
 kD = config.get('gains', 'kD')
-#read from gains to array
+
+#read from client to array
 error = [1, 2, 3, 4, 5]
 timeStamp = [0.1, 0.2, 0.3, 0.4, 0.5]
+seqNum = [1, 2, 3, 4, 5]
+
+#Proportional Controller Effort and Linear Prediction Filter
+
+#Integral Controller Effort and Linear Prediction Filter
+
+#Derivative Controller Effort and Linear Prediction Filter
+
+#Push To Client
+
+
+
