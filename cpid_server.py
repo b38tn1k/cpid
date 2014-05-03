ecc82705c7f1e75bf0c693bd261f3cc58a7783aa
jamesrobertcarthew <jamesrobertcarthew@gmail.com>
2014-05-03 14:37:20 +1000

    cloud server test 1

--- cpid_server.py
+++ cpid_server.py
@@ -5,21 +5,24 @@
 import ConfigParser
 
 #read PID gains from gains.conf
-config = ConfigParse.RawCOnfigParser()
+config = ConfigParser.RawConfigParser()
 config.read('gains.conf')
 kP = config.get('gains', 'kP')
 kI = config.get('gains', 'kI')
-kD = config.get('gains', 'kD')
+kD = config.get('gains', 'kD')  nano
 
 #read from gains to array
 error = [1, 2, 3, 4, 5]
 timeStamp = [0.1, 0.2, 0.3, 0.4, 0.5]
 seqNum = [1, 2, 3, 4, 5]
 
-#Proportional Controller Effort and LPF
+#PID Controller
+#u(t) = kP*e(t) + kI*integral(e(tau)) + kD*(d/dt)e(t)
 
-#Integral Controller Effort and LPF
-
-#Derivative Controller Effort and LPF
+#Proportional Controller Effort and LPF: kP*e(t)
+pCE = kP*error[0]
+#Integral Controller Effort and LPF: kI*integral(e(tau))
+iCE = kI*
+#Derivative Controller Effort and LPF: kD*(d/dt)e(t)
 
 #Push to Client
