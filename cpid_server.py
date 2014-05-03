b1e9a081cfa4e09bf6ceb5d515148a5a1e5aeec1
James Carthew <jamesrobertcarthew@gmail.com>
2014-05-03 12:58:45 +1000

    update cpid_server.py

--- cpid_server.py
+++ cpid_server.py
@@ -10,6 +10,16 @@ config.read('gains.conf')
 kP = config.get('gains', 'kP')
 kI = config.get('gains', 'kI')
 kD = config.get('gains', 'kD')
+
 #read from gains to array
 error = [1, 2, 3, 4, 5]
 timeStamp = [0.1, 0.2, 0.3, 0.4, 0.5]
+seqNum = [1, 2, 3, 4, 5]
+
+#Proportional Controller Effort and LPF
+
+#Integral Controller Effort and LPF
+
+#Derivative Controller Effort and LPF
+
+#Push to Client
