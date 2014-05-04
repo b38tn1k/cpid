c2d1e54fcb5a76c86842b113a060aa53e772b5c0
jamesrobertcarthew <jamesrobertcarthew@gmail.com>
2014-05-04 15:27:32 +1000

    client-server test1

--- cpid_server.py
+++ cpid_server.py
@@ -1,28 +1,34 @@
-#CPID - Cloud Based PID controller (Server)
+#CPID - Cloud Based PID controller
 #James Carthew 2014
 #jamesrobertcarthew@gmail.com
-
 import ConfigParser
 
+def cpid(error, timeStamp, seqNum):
+
 #read PID gains from gains.conf
-config = ConfigParser.RawConfigParser()
-config.read('gains.conf')
-kP = config.get('gains', 'kP')
-kI = config.get('gains', 'kI')
-kD = config.get('gains', 'kD')  nano
-
-#read from gains to array
-error = [1, 2, 3, 4, 5]
-timeStamp = [0.1, 0.2, 0.3, 0.4, 0.5]
-seqNum = [1, 2, 3, 4, 5]
-
+    config = ConfigParser.RawConfigParser()
+    config.read('gains.conf')
+    kP = config.get('gains', 'kP')
+    kI = config.get('gains', 'kI')
+    kD = config.get('gains', 'kD')
+#print Incomming Values
+    print ("Incomming Error:  "+str(error).strip('[]'))
+    print ("Time Stamp:       "+str(timeStamp).strip('[]'))
+    print ("Sequence Number:  "+str(seqNum).strip('[]'))    
 #PID Controller
 #u(t) = kP*e(t) + kI*integral(e(tau)) + kD*(d/dt)e(t)
-
+    
 #Proportional Controller Effort and LPF: kP*e(t)
-pCE = kP*error[0]
+    pCE = kP*error[0]
 #Integral Controller Effort and LPF: kI*integral(e(tau))
-iCE = kI*
+    iCE = kI*sum(error)
 #Derivative Controller Effort and LPF: kD*(d/dt)e(t)
+    dCE = kD*(error[0]-error[1])
+    print pCE
+    print iCE
+    print dCE
+#Return
+    return (pCE+iCE+dCE)
 
-#Push to Client
+effort = cpid([1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4])
+print ("Control Effort:   " + effort)
