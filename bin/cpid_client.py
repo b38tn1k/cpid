f3d8d9e4796d8acfbc6a07eff7253a853ec713f0
jamesrobertcarthew <jamesrobertcarthew@gmail.com>
2014-05-04 16:52:35 +1000

    float stream?

--- bin/cpid_client.py
+++ bin/cpid_client.py
@@ -1,15 +1,22 @@
 import socket
 
-
+#TCP/IP Server Address, Port Number and Buffer Size
 TCP_IP = '119.9.21.113'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
 
+#Make Connections
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+
+values = (1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5)
+packer = struct.Struct('f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, ')
+packed_data = packer.pack(*values)
+
+s.sendall(packed_data)
 data = s.recv(BUFFER_SIZE)
+
+#break
 s.close()
 
 print "received data:", data
--- bin/cpid_server.py
+++ bin/cpid_server.py
@@ -12,11 +12,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
+unpacker = struct.Struct('f, f, f, f, f, f, f, f, f, f, f, f, f, f, f, ')
 conn, addr = s.accept()
 print 'Connection address:', addr
 while 1:
     data = conn.recv(BUFFER_SIZE)
     if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
+    unpacked_data = unpacker.unpack(data)
+    print "received data:", unpacked_data
+    conn.send(unpacked_data)  # echo
 conn.close()
