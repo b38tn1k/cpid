12a0d8e6058ea28ee45452e2b677cde9441ece8d
jamesrobertcarthew <jamesrobertcarthew@gmail.com>
2014-05-11 12:42:27 +1000

    Client Streaming data to Server

--- cpid_client.py
+++ cpid_client.py
@@ -2,23 +2,27 @@ import binascii
 import socket
 import struct
 import sys
+import time
 
 # Create a TCP/IP socket
 TCP_IP = '119.9.21.113'
 TCP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((TCP_IP, TCP_PORT))
-#pack error, time code, sequence number into a struct to simplify send
-values = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5)
-packer = struct.Struct('15f')
-packed_data = packer.pack(*values)
-
-try:
+while True:
+    #pack error, time code, sequence number into a struct to simplify send
+    values = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5)
+    packer = struct.Struct('15f')
+    packed_data = packer.pack(*values)
     
-    # Send data
-    print >>sys.stderr, 'sending "%s"' % binascii.hexlify(packed_data), values
-    sock.sendall(packed_data)
+    try:
+        # Send data
+        print >>sys.stderr, 'sending...'
+        sock.send(packed_data)
+        
+    finally:
+        print >>sys.stderr, 'closing socket'
+        sock.close()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((TCP_IP, TCP_PORT))
 
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
