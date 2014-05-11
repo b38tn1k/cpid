c79c085a65052e5aae8c923ead5d9406f3d6fac8
jamesrobertcarthew <jamesrobertcarthew@gmail.com>
2014-05-11 16:35:03 +1000

    cleaning

--- cpid_client.py
+++ cpid_client.py
@@ -5,11 +5,11 @@ import sys
 import time
 
 # Create a TCP/IP socket
-TCP_IP = '119.9.21.113'
+TCP_IP = 'cpid.io'
 TCP_PORT = 5005
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((TCP_IP, TCP_PORT))
 while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((TCP_IP, TCP_PORT))
     #pack error, time code, sequence number into a struct to simplify send
     values = (1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5)
     packer = struct.Struct('15f')
@@ -19,10 +19,11 @@ while True:
         # Send data
         print >>sys.stderr, 'sending...'
         sock.send(packed_data)
+        confirm = sock.recv(3);
+        print confirm
         
     finally:
         print >>sys.stderr, 'closing socket'
         sock.close()
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((TCP_IP, TCP_PORT))
+
 
